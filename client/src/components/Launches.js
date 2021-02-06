af3bcb4a854543d83c63aa3a0a0ec3b31bf7cf20
flaaaps <julianwening4@gmail.com>
2021-02-06 20:35:42 +0100

    Format all files with prettier

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,53 +1,51 @@
-import React, { Fragment, useState } from 'react';
-import gql from 'graphql-tag';
-import { useQuery } from 'react-apollo';
-import LaunchItem from './LaunchItem';
-import MissionKey from './MissionKey';
+import React, { Fragment, useState } from "react";
+import gql from "graphql-tag";
+import { useQuery } from "react-apollo";
+import LaunchItem from "./LaunchItem";
+import MissionKey from "./MissionKey";
 
 const LAUNCHES_QUERY = gql`
-	query LaunchesQuery {
-		launches {
-			flight_number
-			mission_name
-			launch_date_local
-			launch_success
-		}
-	}
+    query LaunchesQuery {
+        launches {
+            flight_number
+            mission_name
+            launch_date_local
+            launch_success
+        }
+    }
 `;
 
 function Launches() {
-	const { loading, error, data } = useQuery(LAUNCHES_QUERY);
-	const [launchItems, setLaunchItems] = useState([]);
-	const [launchItemsOrder, setLaunchItemsOrder] = useState('desc');
+    const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+    const [launchItems, setLaunchItems] = useState([]);
+    const [launchItemsOrder, setLaunchItemsOrder] = useState("desc");
 
-	if (loading) return <h1 className="display-4 my-3">Loading...</h1>;
-	if (error) console.log(error);
+    if (loading) return <h1 className="display-4 my-3">Loading...</h1>;
+    if (error) console.log(error);
 
-	if (data && launchItems.length === 0) {
-		setLaunchItems(data.launches);
-	}
+    if (data && launchItems.length === 0) {
+        setLaunchItems(data.launches);
+    }
 
-	function updateItemsOrder() {
-		setLaunchItems(launchItems.reverse());
-		setLaunchItemsOrder(prevLaunchItemsOrder =>
-			prevLaunchItemsOrder === 'asc' ? 'desc' : 'asc'
-		);
-	}
+    function updateItemsOrder() {
+        setLaunchItems(launchItems.reverse());
+        setLaunchItemsOrder((prevLaunchItemsOrder) => (prevLaunchItemsOrder === "asc" ? "desc" : "asc"));
+    }
 
-	return (
-		<Fragment>
-			<h1 className="display-4 my-3">Launches</h1>
-			<MissionKey />
-			<button className="btn btn-secondary mb-4" onClick={updateItemsOrder}>
-				<strong>Order by date: </strong>
-				{launchItemsOrder === 'asc' ? 'Ascending' : 'Descending'}
-			</button>
-			<Fragment>
-				{launchItems.map(launch => (
-					<LaunchItem key={launch.flight_number} launch={launch} />
-				))}
-			</Fragment>
-		</Fragment>
-	);
+    return (
+        <Fragment>
+            <h1 className="display-4 my-3">Launches</h1>
+            <MissionKey />
+            <button className="btn btn-secondary mb-4" onClick={updateItemsOrder}>
+                <strong>Order by date: </strong>
+                {launchItemsOrder === "asc" ? "Ascending" : "Descending"}
+            </button>
+            <Fragment>
+                {launchItems.map((launch) => (
+                    <LaunchItem key={launch.flight_number} launch={launch} />
+                ))}
+            </Fragment>
+        </Fragment>
+    );
 }
 export default Launches;
--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -1,49 +1,34 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
+import React, { useState } from "react";
+import classNames from "classnames";
 
 function Note() {
-	const [showModal, setShowModal] = useState(true);
-	return (
-		<div
-			className={classNames(
-				'alert',
-				'alert-warning',
-				'alert-dismissible',
-				'fade',
-				'm-4',
-				'fixed-bottom',
-				'position-fixed',
-				{
-					show: showModal
-				}
-			)}
-			style={{ width: '95%' }}
-			role="alert"
-		>
-			<strong>Note!</strong> This page isn't hosted by{' '}
-			<a href="https://traversymedia.com" target="_blank" rel="noreferrer">
-				Traversy Media
-			</a>{' '}
-			anymore, it is a modified version of Brad's React GraphQL{' '}
-			<a href="https://www.youtube.com/playlist?list=PLillGF-RfqbZrjw48EXLdM4dsOhURCLZx" target="_blank" rel="noreferrer">
-				course
-			</a>
-			. If you (Brad) do not like this idea, please contact me on{' '}
-			<a href="https://twitter.com/jvliix" target="_blank" rel="noreferrer">
-				Twitter
-			</a>
-			.
-			<button
-				type="button"
-				className="close"
-				onClick={() => setShowModal(false)}
-				data-dismiss="alert"
-				aria-label="Close"
-			>
-				<span aria-hidden="true">&times;</span>
-			</button>
-		</div>
-	);
+    const [showModal, setShowModal] = useState(true);
+    return (
+        <div
+            className={classNames("alert", "alert-warning", "alert-dismissible", "fade", "my-4", "fixed-bottom", "position-fixed", {
+                show: showModal,
+            })}
+            style={{ width: "95%" }}
+            role="alert"
+        >
+            <strong>Note!</strong> This page isn't hosted by{" "}
+            <a href="https://traversymedia.com" target="_blank" rel="noreferrer">
+                Traversy Media
+            </a>{" "}
+            anymore, it is a modified version of Brad's React GraphQL{" "}
+            <a href="https://www.youtube.com/playlist?list=PLillGF-RfqbZrjw48EXLdM4dsOhURCLZx" target="_blank" rel="noreferrer">
+                course
+            </a>
+            . If you (Brad) do not like this idea, please contact me on{" "}
+            <a href="https://twitter.com/jvliix" target="_blank" rel="noreferrer">
+                Twitter
+            </a>
+            .
+            <button type="button" className="close" onClick={() => setShowModal(false)} data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    );
 }
 
 export default Note;
